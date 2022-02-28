5d9509863cff7dc2316ff1634c4e56cb6259b323
Eric Phifer <ericoncode@gmail.com>
2022-02-28 16:39:06 -0700

    adjust side margin on testimonial page to make fully responsive

--- gatsby-jc/src/pages/testimonials.js
+++ gatsby-jc/src/pages/testimonials.js
@@ -5,7 +5,7 @@ import SEO from '../components/SEO';
 
 const TestimonialStyles = styled.div`
   max-width: 1200px;
-  margin: 5rem auto;
+  margin: 5rem 1.5rem 0;
   padding: 5rem 0;
   text-align: left;
   // Three Column Grid
