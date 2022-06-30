a0f88440b808c432e5c5298ab5c24e1704f9c5b6
Eric Phifer <ericoncode@gmail.com>
2022-06-30 11:42:46 -0600

    add referral page

---
+++ gatsby-jc/src/pages/referral.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import styled from 'styled-components';
+import SEO from '../components/SEO';
+
+const IntakeStyles = styled.div`
+  max-width: 1200px;
+  margin: 5rem 1.5rem 0;
+  padding: 5rem 0;
+  text-align: left;
+  display: flex;
+  justify-content: center;
+  iframe {
+    width: 100%;
+  }
+  // Three Column Grid
+  .threeColContainer {
+    display: grid;
+    grid-template-columns: repeat(3, minmax(auto, 1fr));
+    gap: 1rem;
+    margin: 0 auto;
+    padding: 2rem 0;
+    place-content: center;
+    @media only screen and (max-width: 450px) {
+      grid-template-columns: repeat(2, minmax(auto, 1fr));
+    }
+    @media only screen and (max-width: 300px) {
+      grid-template-columns: minmax(auto, 1fr);
+    }
+  }
+  .threeCol {
+  }
+  @media only screen and (max-width: 400px) {
+    margin: 5rem 1.5rem;
+  }
+`;
+
+export default function Intake() {
+  return (
+    <>
+      <SEO title="Intake Form" />
+      <IntakeStyles>
+        <iframe
+          src="https://the-joseph-center.casemgr.org/embed-casemgr-referral/46dd9429268ede42bf0f31629fb5fcea1fd65236"
+          title="Intake Form"
+          frameBorder="0"
+          width="800"
+          height="800"
+        />
+      </IntakeStyles>
+    </>
+  );
+}
