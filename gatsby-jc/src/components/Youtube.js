4d6d8088f80accd5369f753af1e1ac11f27a9728
Eric Phifer <ericoncode@gmail.com>
2022-07-19 16:16:35 -0600

    toggle and checkbox failed to show users response in email notification - changed to select field instead

--- gatsby-jc/src/components/Youtube.js
+++ gatsby-jc/src/components/Youtube.js
@@ -1,41 +1,163 @@
-import React from "react"
-import PropTypes from "prop-types"
-import styled from "styled-components"
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 const StyledYouTube = styled.div`
-    overflow: hidden;
-    position: relative;
+  overflow: hidden;
+  position: relative;
+  width: 100%;
+  ::after {
+    padding-top: 56.25%;
+    display: block;
+    content: '';
+  }
+  iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
     width: 100%;
-    ::after {
-        padding-top: 56.25%;
-        display: block;
-        content: "";
-    }
-    iframe {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-    }
-`
+    height: 100%;
+  }
+  /* Spare Code */
+  /* Customize the label (the container) */
+  .checkboxLabel {
+    display: block;
+    position: relative;
+    padding-left: 35px;
+    margin-bottom: 12px;
+    cursor: pointer;
+    font-size: 22px;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+  }
 
-const YouTube = ({id}) => {
-    return (
-        <StyledYouTube>
-            <iframe
-                title={id}
-                src={`https://www.youtube-nocookie.com/embed/${id}`}
-                frameBorder="0"
-                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-            />
-        </StyledYouTube>
-    )
-}
+  /* Hide the browser's default checkbox */
+  .checkboxLabel input {
+    position: absolute;
+    opacity: 0;
+    cursor: pointer;
+    height: 25px;
+    width: 25px;
+    left: 0;
+    top: -5px;
+  }
+  /* Create a custom checkbox */
+  .checkmark {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 25px;
+    width: 25px;
+    background-color: #eee;
+  }
+
+  /* Create a Toggle switch*/
+  .switch {
+    position: relative;
+    display: inline-block;
+    width: 77px;
+    height: 34px;
+  }
+  .switch input {
+    opacity: 0;
+    width: 0;
+    height: 0;
+  }
+  .slider {
+    position: absolute;
+    cursor: pointer;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #ccc;
+    -webkit-transition: 0.4s;
+    transition: 0.4s;
+  }
+
+  .slider:before {
+    position: absolute;
+    content: '';
+    height: 26px;
+    width: 26px;
+    left: 4px;
+    bottom: 4px;
+    background-color: white;
+    -webkit-transition: 0.4s;
+    transition: 0.4s;
+  }
+
+  input:checked + .slider {
+    background-color: #2196f3;
+  }
+
+  input:focus + .slider {
+    box-shadow: 0 0 1px #2196f3;
+  }
+
+  input:checked + .slider:before {
+    -webkit-transform: translateX(26px);
+    -ms-transform: translateX(26px);
+    transform: translateX(26px);
+  }
+  .slider.round {
+    border-radius: 34px;
+  }
+  .slider.round:before {
+    border-radius: 50%;
+  }
+
+  /* On mouse-over, add a grey background color */
+  .checkboxLabel:hover input ~ .checkmark {
+    background-color: #ccc;
+  }
+
+  /* When the checkbox is checked, add a blue background */
+  .checkboxLabel input:checked ~ .checkmark {
+    background-color: #2196f3;
+  }
+
+  /* Create the checkmark/indicator (hidden when not checked) */
+  .checkmark:after {
+    content: '';
+    position: absolute;
+    display: none;
+  }
+
+  /* Show the checkmark when checked */
+  .checkboxLabel input:checked ~ .checkmark:after {
+    display: block;
+  }
+  /* Style the checkmark/indicator */
+  .checkboxLabel .checkmark:after {
+    left: 9px;
+    top: 5px;
+    width: 5px;
+    height: 10px;
+    border: solid white;
+    border-width: 0 3px 3px 0;
+    -webkit-transform: rotate(45deg);
+    -ms-transform: rotate(45deg);
+    transform: rotate(45deg);
+  }
+`;
+
+const YouTube = ({ id }) => (
+  <StyledYouTube>
+    <iframe
+      title={id}
+      src={`https://www.youtube-nocookie.com/embed/${id}`}
+      frameBorder="0"
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+    />
+  </StyledYouTube>
+);
 
 YouTube.propTypes = {
-    id: PropTypes.string.isRequired,
-}
+  id: PropTypes.string.isRequired,
+};
 
 export default YouTube;
--- gatsby-jc/src/pages/schoolsupplies.js
+++ gatsby-jc/src/pages/schoolsupplies.js
@@ -44,135 +44,6 @@ const FormStyles = styled.div`
       padding: 1vmin;
       margin: 1vmin 0;
     }
-    .inline {
-      display: inline-flex;
-      flex-flow: row nowrap;
-    }
-
-    /* Customize the label (the container) */
-    .checkboxLabel {
-      display: block;
-      position: relative;
-      padding-left: 35px;
-      margin-bottom: 12px;
-      cursor: pointer;
-      font-size: 22px;
-      -webkit-user-select: none;
-      -moz-user-select: none;
-      -ms-user-select: none;
-      user-select: none;
-    }
-
-    /* Hide the browser's default checkbox */
-    .checkboxLabel input {
-      position: absolute;
-      opacity: 0;
-      cursor: pointer;
-      height: 25px;
-      width: 25px;
-      left: 0;
-      top: -5px;
-    }
-
-    /* Create a custom checkbox */
-    .checkmark {
-      position: absolute;
-      top: 0;
-      left: 0;
-      height: 25px;
-      width: 25px;
-      background-color: #eee;
-    }
-
-    /* Create a Toggle switch*/
-    .switch {
-      position: relative;
-      display: inline-block;
-      width: 77px;
-      height: 34px;
-    }
-    .switch input {
-      opacity: 0;
-      width: 0;
-      height: 0;
-    }
-    .slider {
-      position: absolute;
-      cursor: pointer;
-      top: 0;
-      left: 0;
-      right: 0;
-      bottom: 0;
-      background-color: #ccc;
-      -webkit-transition: 0.4s;
-      transition: 0.4s;
-    }
-
-    .slider:before {
-      position: absolute;
-      content: '';
-      height: 26px;
-      width: 26px;
-      left: 4px;
-      bottom: 4px;
-      background-color: white;
-      -webkit-transition: 0.4s;
-      transition: 0.4s;
-    }
-
-    input:checked + .slider {
-      background-color: #2196f3;
-    }
-
-    input:focus + .slider {
-      box-shadow: 0 0 1px #2196f3;
-    }
-
-    input:checked + .slider:before {
-      -webkit-transform: translateX(26px);
-      -ms-transform: translateX(26px);
-      transform: translateX(26px);
-    }
-    .slider.round {
-      border-radius: 34px;
-    }
-    .slider.round:before {
-      border-radius: 50%;
-    }
-
-    /* On mouse-over, add a grey background color */
-    .checkboxLabel:hover input ~ .checkmark {
-      background-color: #ccc;
-    }
-
-    /* When the checkbox is checked, add a blue background */
-    .checkboxLabel input:checked ~ .checkmark {
-      background-color: #2196f3;
-    }
-
-    /* Create the checkmark/indicator (hidden when not checked) */
-    .checkmark:after {
-      content: '';
-      position: absolute;
-      display: none;
-    }
-
-    /* Show the checkmark when checked */
-    .checkboxLabel input:checked ~ .checkmark:after {
-      display: block;
-    }
-    /* Style the checkmark/indicator */
-    .checkboxLabel .checkmark:after {
-      left: 9px;
-      top: 5px;
-      width: 5px;
-      height: 10px;
-      border: solid white;
-      border-width: 0 3px 3px 0;
-      -webkit-transform: rotate(45deg);
-      -ms-transform: rotate(45deg);
-      transform: rotate(45deg);
-    }
     h3 {
       margin-top: 3vmin;
     }
@@ -299,22 +170,20 @@ export default function SchoolSupplies() {
                 onChange={updateValue}
                 required
               />
-              <fieldset className="inline">
-                <label htmlFor="assistwords">
-                  Toggle the switch to blue if you will need assistance with the
-                  school fee
-                </label>
-                <label htmlFor="assistance" className="switch">
-                  <input
-                    name="assistance"
-                    id="assistance"
-                    type="checkbox"
-                    value={values.assistance}
-                    onChange={updateValue}
-                  />
-                  <span className="slider round" />
-                </label>
-              </fieldset>
+              <label htmlFor="assistance" className="assistance">
+                Will you need assistance with the school fee?
+              </label>
+              <select
+                name="assistance"
+                id="assistance"
+                type="assistance"
+                value={values.assistance}
+                onChange={updateValue}
+                required
+              >
+                <option value="Yes">Yes</option>
+                <option value="No">No</option>
+              </select>
               <button type="submit" value="Submit">
                 Submit Application
               </button>
